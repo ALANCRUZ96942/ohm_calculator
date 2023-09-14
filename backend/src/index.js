7c79222d916d7b9bad29d7485fbb717c7028b26a
ALANCRUZ96942 <alan96942@gmail.com>
2023-09-14 02:29:05 -0600

    chore: add sequelize and path

--- backend/src/index.js
+++
@@ -1,8 +0,0 @@
-import app from './server'
-const PORT = process.env.PORT;
-(function main(){
-    app.listen(PORT, function() {
-        console.log("App listening on PORT " + PORT);
-    });
-})();
-    
